96b4f80bd9e99ca27c486143bf03a9d2a1549cdd
sdevore <sdevore@gmail.com>
2016-03-12 16:38:33 -0700

    New Base Project

---
+++ PT_ReleaseNotes/PT_ReleaseNotes/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  PT_ReleaseNotes
+//
+//  Created by Samuel DeVore on 3/12/16.
+//  Copyright © 2016 Samuel DeVore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
